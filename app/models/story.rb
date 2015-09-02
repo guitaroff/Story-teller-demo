32937e998c5c69460e864c771df74a85be857892
andrey <ag82pishi@yandex.ru>
2015-09-02 23:23:34 +0300

    owner 'current user' or 'guest'

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,9 +1,9 @@
 class Story < ActiveRecord::Base
+  include PublicActivity::Model
+  tracked owner: Proc.new { |controller, model| controller.current_user ? controller.current_user : nil }
+
   belongs_to :user
   
   validates :title, presence: true
   validates :body, presence: true
-
-  include PublicActivity::Model
-  tracked
 end
